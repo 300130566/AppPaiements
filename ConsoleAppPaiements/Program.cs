be3a3c93ce98cea744bb3e61a8d036ee1031e5f3
Sadissou Chefik Olabissi Ichola <300130566@monboreal.ca>
2024-10-24 12:30:16 -0400

    Ajout de la classe CarteCredit et création de ses instances

--- ConsoleAppPaiements/Program.cs
+++ ConsoleAppPaiements/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using ConsoleAppPaiements;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CarteCredit achat1 = new CarteCredit(29.99, "Achat de vetements sur ASOS", 987654321);
+        CarteCredit achat2 = new CarteCredit(99.99, "Achat d'un ordinateur sur Lenovo", 987654321);
+        CarteCredit achat3 = new CarteCredit(250.00, "Achat d'un nouvel appareil électronique sur Amazone", 987654530);
+
+        achat1.AfficherDetails();
+        achat2.AfficherDetails();
+        achat3.AfficherDetails();
     }
 }
