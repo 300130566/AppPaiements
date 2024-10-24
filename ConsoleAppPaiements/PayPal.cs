ed4ca0aa9a045a5e3b2ee1369b677cf1003124b1
Sadissou Chefik Olabissi Ichola <300130566@monboreal.ca>
2024-10-24 13:00:02 -0400

    Ajout de la classe PayPal et création de ses instances

---
+++ ConsoleAppPaiements/PayPal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPaiements
+{
+    public class PayPal: Paiement
+    {
+        public string Courriel { get; set; }
+        public PayPal(double montant, string description, string courriel)
+        : base(montant, description)
+        {
+            Courriel = courriel;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Montant: {Montant}, Description: {Description}, Courriel: {Courriel}");
+        }
+    }
+}
--- ConsoleAppPaiements/Program.cs
+++ ConsoleAppPaiements/Program.cs
@@ -5,11 +5,18 @@ internal class Program
     private static void Main(string[] args)
     {
         CarteCredit achat1 = new CarteCredit(29.99, "Achat de vetements sur ASOS", 987654321);
-        CarteCredit achat2 = new CarteCredit(99.99, "Achat d'un ordinateur sur Lenovo", 987654321);
+        CarteCredit achat2 = new CarteCredit(1199.99, "Achat d'un ordinateur sur Lenovo", 987654321);
         CarteCredit achat3 = new CarteCredit(250.00, "Achat d'un nouvel appareil électronique sur Amazone", 987654530);
+
+        PayPal payment1 = new PayPal(59.99, "Achat en ligne", "300130566@monboreal.ca");
+        PayPal payment2 = new PayPal(19.99, "Abonnement mensuel", "300130566@monboreal.ca");
 
         achat1.AfficherDetails();
         achat2.AfficherDetails();
         achat3.AfficherDetails();
+
+        payment1.AfficherDetails();
+        payment2.AfficherDetails();
+
     }
 }
